c9d90fed1bf49e3e5bbd5033abbcd2aded617ec5
Vansh <vanshchoudhary2744@gmail.com>
2026-05-28 15:56:08 +0530

    uncomment array initialization and related operations in array_test.py

--- array_test.py
+++ array_test.py
@@ -1,10 +1,10 @@
 import array
 
-# numbers = array.array('i',[1,2,3,4,5])
+numbers = array.array('i',[1,2,3,4,5])
 
 # numbers = array.array('d',[1,2,3,4,5])
 
-numbers = array.array('w',['a','b','c','d','e'])
+# numbers = array.array('w',['a','b','c','d','e'])
 
 # print(numbers[0])
 
@@ -14,12 +14,25 @@ numbers = array.array('w',['a','b','c','d','e'])
 # print()
 # print(numbers.typecode)
 
-numbers.append('f')
+# numbers.append('f')
 
-reverse_arr = array.array(numbers.typecode, [])
+# reverse_arr = array.array(numbers.typecode, [])
 
-for i in range(len(numbers)-1, -1, -1):
-    print(i)
-    reverse_arr.append(numbers[i])
+# for i in range(len(numbers)-1, -1, -1):
+#     print(i)
+#     reverse_arr.append(numbers[i])
 
-print(reverse_arr)
+# print(reverse_arr)
+
+numbers.insert(2, 10)
+numbers.remove(4)
+numbers.append(6)
+numbers.pop(0)
+
+numbers[1] = 20
+
+print(numbers)
+
+copy_array = array.array(numbers.typecode, [num for num in numbers])
+
+print(copy_array)
