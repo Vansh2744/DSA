cf42a693268849a0adef463b71635e99a06fadaa
Vansh <vanshchoudhary2744@gmail.com>
2026-05-28 17:18:05 +0530

    update array_numpy.py to demonstrate multi-dimensional numpy array creation

--- array_numpy.py
+++ array_numpy.py
@@ -1,7 +1,27 @@
 import numpy as np
 
-arr = np.array([1, 2, 3, 4, 5, 6.5, 'a', 'b', 'c'])
+# arr = np.array([1, 2, 3, 4, 5, 6.5, 'a', 'b', 'c'])
 
-for i in arr:
-    print(i, end=" ")
+# arr = np.array([1, 2, 3, 4, 5], float)
 
+# arr = np.array([1, 2, 3, 4, 5, 6.5], int)
+
+# arr = np.linspace(1,11,5)
+
+# arr = np.arange(1, 11, 2)
+
+# arr = np.zeros(10)
+
+# arr = np.ones(10, int)
+
+# arr = np.full(10, 4)
+
+# for i in arr:
+#     print(i, end=" ")
+
+
+# arr = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+
+arr = np.array([[[1,2],[3,4]],[[5,6],[7,8]]])
+
+print(arr)
