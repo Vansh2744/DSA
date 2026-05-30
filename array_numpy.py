98a5426730d9ea2bf78dbd66deacca292f87f237
Vansh <vanshchoudhary2744@gmail.com>
2026-05-30 23:20:22 +0530

    solved test questions

--- array_numpy.py
+++ array_numpy.py
@@ -24,4 +24,13 @@ import numpy as np
 
 arr = np.array([[[1,2],[3,4]],[[5,6],[7,8]]])
 
-print(arr)
+# print(arr)
+
+res = []
+
+res.append(2)
+res.append(3)
+res.append(4)
+
+res.remove(3)
+print(res)
