884e5892d3e4a018dade52f13486cf7c4a95d68d
Vansh <vanshchoudhary2744@gmail.com>
2026-07-01 09:45:02 +0530

    solved minimum denominator problem

--- greedy_algo.py
+++ greedy_algo.py
@@ -1,3 +1,5 @@
+from collections import abc
+from collections import abc
 def max_sum(arr):
     sorted_arr = arr.sort()
     i = 0
@@ -25,5 +27,23 @@ def min_sum(arr):
 
 arr = [3,5,10,2,6,9]
 
-print(f"Maximum Sum : {max_sum(arr)}")
-print(f"Minimum Sum : {min_sum(arr)}")
+# print(f"Maximum Sum : {max_sum(arr)}")
+# print(f"Minimum Sum : {min_sum(arr)}")
+
+def min_denominator(arr, amount):
+    coins = arr.sort()
+    balance = amount
+    i = len(arr)-1
+    count = 0
+
+    while balance != 0:
+        if arr[i] <= balance:
+            balance -= arr[i]
+            count += 1
+        else:
+            i-=1
+    return count
+
+arr = [2,10,20,30,40,50]
+
+print(min_denominator(arr, 224))
